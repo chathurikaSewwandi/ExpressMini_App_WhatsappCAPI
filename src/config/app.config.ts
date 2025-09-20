5360e5574bfb4d852dbb0592470a70cc404ed79b
csewwa <avcsewwandi@std.ssl.sab.ac.lk>
2025-09-20 12:02:27 +0530

    Update app configuration: replace hardcoded PORT with environment variable, and add OPEN_AI_KEY and GEMINI_API_KEY for enhanced flexibility.

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -7,6 +7,8 @@ export const APP_CONFIG = {
     VERSION: process.env.VERSION,
     WHATSAPP_USER_ACCESS_TOKEN: process.env.WHATSAPP_USER_ACCESS_TOKEN,
     WEBHOOK_VERIFICATION_PASSWORD: process.env.WEBHOOK_VERIFICATION_PASSWORD,
-    PORT: 5885
+    PORT:process.env.PORT,
+    OPEN_AI_KEY:process.env.OPEN_AI_KEY,
+    GEMINI_API_KEY:process.env.GEMINI_KEY
     
 }
